76414005a31caa6b76afe6d71497448621941cdb
tusharbhambere <tusharbhambere8@gmail.com>
2021-06-27 11:08:40 +0530

    changes

--- src/ormconfid.ts
+++ src/ormconfid.ts
@@ -1,15 +1,19 @@
 import { join } from "path";
 import { ConnectionOptions } from "typeorm";
 import { Todo } from "./database/Entity/todo.entity";
+import dotenev from "dotenv"
 
 
+dotenev.config()
 const connectionOptions: ConnectionOptions = {
+  url: process.env.DATABASE_URL,
+  ssl :{rejectUnauthorized:false},
   type: "postgres",
-  host: "localhost",
+  host: process.env.Host || "localhost",
   port: 5432,
-  username: "tushar111",
-  password: "Bunty@123",
-  database: "postgres",
+  username: process.env.User ||  "tushar111",
+  password: process.env.DB_Password ||  "Bunty@123",
+  database: process.env.Database ||  "postgres",
   entities: [Todo],
   synchronize: true,
   dropSchema: false,
